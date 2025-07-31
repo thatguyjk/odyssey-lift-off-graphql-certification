1c2b30308078cc8109c4926aa60a80e68774aa78
Jonathan Kelly <JonathanK.Kelly@gmail.com>
2025-07-31 11:55:08 -0400

    updating the server to use the resolvers and data sources

--- server/src/index.js
+++ server/src/index.js
@@ -3,36 +3,26 @@ const { startStandaloneServer } = require("@apollo/server/standalone");
 const { addMocksToSchema } = require("@graphql-tools/mock");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const TrackAPI = require("./datasources/track-api");
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
-    }),
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
 
   console.log(`
     *********************
